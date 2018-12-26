31acb07cc977ce68b4f739a1b2cd2d613619d93a
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2018-12-26 17:41:29 -0500

    added update and delete options

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -18,3 +18,33 @@ export const createProject = (project) => {
         })    
     }
 }
+
+export const deleteProject = (project) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        // make async call to database
+        const firestore = getFirestore();
+            
+        firestore.collection('projects').doc(project).delete()
+            .then(() => {
+                dispatch({ type: 'DELETE_PROJECT', project });
+            }).catch((err) => {
+                dispatch({ type: 'DELETE_PROJECT_ERROR' }, err);
+            }) 
+    }
+}
+
+export const editProject = (project) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        // make async call to database
+        const firestore = getFirestore();
+            
+        firestore.collection('projects').doc(project.id).update({
+            title: project.title,
+            content: project.content
+        }).then(() => {
+            dispatch({ type: 'EDIT_PROJECT', project });
+        }).catch((err) => {
+            dispatch({ type: 'EDIT_PROJECT_ERROR' }, err);
+        }) 
+    }
+}
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,21 +1,29 @@
 const initState = {
-  projects: [
-    {id: '1', title: 'help me find peach', content: 'blah blah blah'},
-    {id: '2', title: 'collect all the stars', content: 'blah blah blah'},
-    {id: '3', title: 'egg hunt with yoshi', content: 'blah blah blah'}
-  ]
+  projects: []
 };
 
 const projectReducer = (state = initState, action) => {
     switch (action.type) {
-      case 'CREATE_PROJECT':
-          console.log('created ', action.project);
-          return state;
-      case 'CREATE_PROJECT_ERROR':
-          console.log('create project error', action.err);
-          return state;
-      default:
-        return state;
+        case 'CREATE_PROJECT':
+            console.log('created ', action.project);
+            return state;
+        case 'CREATE_PROJECT_ERROR':
+            console.log('create project error', action.err);
+            return state;
+        case 'DELETE_PROJECT':
+            console.log('deleted ', action.project);
+            return state;
+        case 'DELETE_PROJECT_ERROR':
+            console.log('delete project error', action.err);
+            return state;
+        case 'EDIT_PROJECT':
+            console.log('edited ', action.project);
+            return state;
+        case 'EDIT_PROJECT_ERROR':
+            console.log('edit project error', action.err);
+            return state;
+        default:
+            return state;
     }
   
 };
